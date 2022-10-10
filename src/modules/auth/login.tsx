395e757a6f44dc7b7d7148425f6d99a331025920
htet <htetarkarkyaw2k@gmail.com>
2022-10-10 12:50:37 +0630

    log form created

--- src/modules/auth/login.tsx
+++ src/modules/auth/login.tsx
@@ -1,9 +1,46 @@
 
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import { Checkbox } from 'primereact/checkbox';
+import 'primeicons/primeicons.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import React, { useEffect, useState } from 'react';
+
+
 export const Login = () => {
-    
+    const [checked,setChecked1] = useState(false);
+
     return(
-        <>
-            <h3> Login Page </h3>
-        </>
+       
+        <div className="m-auto static surface-card p-4 shadow-2 border-round w-full lg:w-6">
+            <div className="text-center mb-5">
+                <img src="default-logo.jpg" alt="logo" height={5} className="mb-3" />
+                <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
+                <span className="text-600 font-medium line-height-3">Don't have an account?</span>
+                <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
+            </div>
+
+            <div>
+                <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
+                <InputText type="text" className="w-full mb-3" />
+
+                <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
+                <InputText type="password" className="w-full mb-3" />
+
+                <div className="flex align-items-center justify-content-between mb-6">
+                    <div className="flex align-items-center">
+                        <Checkbox id="rememberme" className="mr-2" checked={checked} onChange={(e) => setChecked1(e.checked)} />
+                        <label htmlFor="rememberme">Remember me</label>
+                    </div>
+                    <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot your password?</a>
+                </div>
+
+                <Button label="Sign In" icon="pi pi-user" className="w-full" />
+            </div>
+        </div>
+            
+            
     )
 }
